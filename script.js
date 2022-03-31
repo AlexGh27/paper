aed3917963ce4f8df800f4110c31a3b34e44df11
Alex <alexghioca27@gmail.com>
2022-03-31 22:49:08 +0300

    Final?

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function computerPlay() {
 
 function playRound(playerSelection, computerSelection) {
      
-    playerSelection = document.getElementById("myText").value;
+    playerSelection = prompt();
     playerValue = playerSelection.toUpperCase();
 
     computerSelection = computerPlay();
@@ -27,38 +27,77 @@ function playRound(playerSelection, computerSelection) {
     let p = playerValue;
     let c = computerValue;
     let result = '';
+    let score = 0;
+    
 
     if (p == 'ROCK' && c == 'PAPER') {
         result = 'You lose! Paper beats Rock';
+        score = 1;
+        console.log(result);
     }
     else if (p == 'ROCK' && c == 'SCISSORS') {
-        result = 'You Win! Rock beats Scissors'
+        result = 'You Win! Rock beats Scissors';
+        score = 3;
+        console.log(result);
     }
     else if (p == 'ROCK' && c == 'ROCK') {
         result = 'Draw!';
+        score = 2;
+        console.log(result);
     }
 
     else if (p == 'PAPER' && c == 'ROCK') {
-        result = 'You win! Paper beats Rock' ;
+        result = 'You win! Paper beats Rock';
+        score = 3;
+        console.log(result);
     }
     else if (p == 'PAPER' && c == 'Scissors') {
         result = 'You lose! Scissors beats Paper!';
+        score = 1;
+        console.log(result);
     }
     else if (p == 'PAPER' && c == 'PAPER') {
         result = 'Draw!';
+        score = 2;
+        console.log(result);
     }
 
     else if (p == 'SCISSORS' && c == 'PAPER') {
         result = 'You win! Scissors beats Paper';
+        score = 3;
+        console.log(result);
     }
     else if (p == 'SCISSORS' && c == 'ROCK') {
         result = 'You lose! Rock beats Scissors';
+        score = 1;
+        console.log(result);
     }
     else if (p == 'SCISSORS' && c == 'SCISSORS') {
         result = 'Draw!';
+        score = 2;
+        console.log(result);
     }
-    console.log(result); 
+    return score; 
 }   
+
+function game() {
+
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for(let i = 0; i < 5; i++) {
+        let score = playRound();
+
+        if (score == 3) {
+            playerScore++;
+        }
+        if (score == 1) {
+            computerScore++;
+        }
+
+    }
+    console.log(`Final Score: You: ${playerScore} , Computer: ${computerScore}`);
+}
 
 
 
